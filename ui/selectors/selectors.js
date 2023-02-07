bcc3f3d5466cc8e5e56a601945db86f67634ae1a
Victor Thomas <10986371+vthomas13@users.noreply.github.com>
2023-02-07 17:23:04 -0500

    lintfix

--- ui/selectors/selectors.js
+++ ui/selectors/selectors.js
@@ -453,10 +453,11 @@ export function getAccountsWithLabels(state) {
   return getMetaMaskAccountsOrdered(state).map(
     ({ address, name, balance }) => ({
       address,
-      addressLabel: `${name.length < TRUNCATED_NAME_CHAR_LIMIT
+      addressLabel: `${
+        name.length < TRUNCATED_NAME_CHAR_LIMIT
           ? name
           : `${name.slice(0, TRUNCATED_NAME_CHAR_LIMIT - 1)}...`
-        } (${shortenAddress(address)})`,
+      } (${shortenAddress(address)})`,
       label: name,
       balance,
     }),
@@ -602,8 +603,8 @@ export function getShouldShowFiat(state) {
   const { showFiatInTestnets } = getPreferences(state);
   return Boolean(
     (isMainNet || isCustomNetwork || showFiatInTestnets) &&
-    useCurrencyRateCheck &&
-    conversionRate,
+      useCurrencyRateCheck &&
+      conversionRate,
   );
 }
 
@@ -1358,20 +1359,20 @@ export function getBlockExplorerLinkText(
   if (rpcPrefs.blockExplorerUrl) {
     blockExplorerLinkText = accountDetailsModalComponent
       ? {
-        firstPart: 'blockExplorerView',
-        secondPart: getURLHostName(rpcPrefs.blockExplorerUrl),
-      }
+          firstPart: 'blockExplorerView',
+          secondPart: getURLHostName(rpcPrefs.blockExplorerUrl),
+        }
       : {
-        firstPart: 'viewinExplorer',
-        secondPart: 'blockExplorerAccountAction',
-      };
+          firstPart: 'viewinExplorer',
+          secondPart: 'blockExplorerAccountAction',
+        };
   } else if (isCustomNetwork === false) {
     blockExplorerLinkText = accountDetailsModalComponent
       ? { firstPart: 'etherscanViewOn', secondPart: '' }
       : {
-        firstPart: 'viewOnEtherscan',
-        secondPart: 'blockExplorerAccountAction',
-      };
+          firstPart: 'viewOnEtherscan',
+          secondPart: 'blockExplorerAccountAction',
+        };
   }
 
   return blockExplorerLinkText;
